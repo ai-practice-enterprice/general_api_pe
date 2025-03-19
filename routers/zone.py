1e602149547602226037cb8ce132f7b64bd44f8a
EnteryName <ferrari.lorenzo04@gmail.com>
2025-03-19 01:58:49 +0100

    add zone generation

--- routers/zone.py
+++ routers/zone.py
@@ -4,17 +4,18 @@ from fastapi import APIRouter, Query
 from faker import Faker
 from faker.config import AVAILABLE_LOCALES
 from enum import Enum
+from prisma.models import Zones
 from typing import Annotated
 
 from config import DB_CLIENT
-from database.db_schema import *
+from database.db_schema import Zones as ZonesSchema
 
 
 # API endpoints for zone data ==========================================================================
 router = APIRouter(prefix="/zones", tags=["Zone Data"])
 log = logging.getLogger(__name__)
 
-@router.get("/status",response_model=list[Zones])
+@router.get("/status",response_model=list[ZonesSchema])
 def get_zone_status():
     """
     Get zone data about all zones status and which to check 
@@ -25,9 +26,9 @@ def get_zone_status():
 
     return  resultQuery
 
-@router.get("/register", response_model=list[Zones])
+@router.get("/register", response_model=list[ZonesSchema])
 def get_register_zone_to_db(
-    zone: Annotated[Zones,Query()] = None,
+    zone: Annotated[ZonesSchema,Query()] = None,
 ):
     """
     Add zone to DB
@@ -37,3 +38,28 @@ def get_register_zone_to_db(
     resultQuery = DB_CLIENT.zones.create(zone)
 
     return resultQuery
+
+
+@router.post("/generate")
+async def generate_random_zones(limit: Annotated[int, Query(ge=1, le=100)] = 1):
+    """
+    Generate random zones data for testing
+    """
+
+    fake = Faker(locale="nl_BE")
+
+    log.info(f"Generating {limit} random zones")
+    zones = []
+
+    for _ in range(limit):
+        zone = await Zones.prisma().create({
+            "zoneDescription": fake.sentence(nb_words=8),
+            "zoneName": fake.word(),
+            "zoneAvailable": fake.boolean(chance_of_getting_true=75),
+            "zoneType": fake.random_element(elements=("dropZoneIn", "dropZoneOut")),
+            "zoneCheck": fake.boolean(chance_of_getting_true=25)
+        })
+
+        zones.append(ZonesSchema(**zone.model_dump()))
+
+    return zones
